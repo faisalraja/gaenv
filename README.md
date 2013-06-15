cf8d872397f315685acbf16604d665e94796d7c9
Faisal Raja <faisalparveez@gmail.com>
2013-06-14 17:32:09 -0700

    initial

---
+++ README.md
@@ -0,0 +1,18 @@
+gaenv - Command line utility for managing appengine thirdparty packages
+==================
+
+Example Usage::
+
+  Create a requirements.txt and put all requirements then run
+  $ gaenv
+  For more
+  $ gaenv -h
+  Note that this not run "pip install -r requirements.txt" so you'll
+  have to run it manually first before running this.
+
+
+This will create symbolic links from your default python version packages to
+the current directory that you execute it on. By default it will create
+gaenv_lib folder with all packages in your requirements.txt and will
+ask you if you want this utility to insert the import statement that updates
+your sys.path, or you can just do it manually.
---
+++ setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+from setuptools import setup, find_packages
+import sys
+import os
+
+version = '0.1.1'
+
+
+def main():
+    setup(name='gaenv',
+          version=version,
+          description="Tool for manage packages for google app engine",
+          long_description=open('README.rst').read(),
+          classifiers=[],
+          keywords='app engine google tool',
+          author='Faisal Raja',
+          author_email='support@altlimit.com',
+          url='http://github.com/faisalraja/gaenv',
+          license='MIT',
+          packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+          include_package_data=True,
+          zip_safe=False,
+          install_requires=[],
+          entry_points={
+              "console_scripts": ['gaenv = gaenv:main'],
+          },
+          )
+
+
+if __name__ == '__main__':
+    main()
